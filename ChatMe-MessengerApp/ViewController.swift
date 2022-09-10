7d806b66da0ee688f290524c559a80e645b401d5
Arctanyn <m.dugulubgov@icloud.com>
2022-09-10 12:14:17 +0300

    Initial Commit

---
+++ ChatMe-MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMe-MessengerApp
+//
+//  Created by Малиль Дугулюбгов on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
